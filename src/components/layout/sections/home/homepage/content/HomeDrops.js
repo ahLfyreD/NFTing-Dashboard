79fa812118460e8fd2154866e29a83f9ee528dce
alfred.annie <fredileader@gmail.com>
2022-09-16 12:26:47 +0100

    add more data to the db-json file and texted it on one of the carousel components

--- src/components/layout/sections/home/homepage/content/HomeDrops.js
+++ src/components/layout/sections/home/homepage/content/HomeDrops.js
@@ -4,27 +4,46 @@ import "react-multi-carousel/lib/styles.css";
 import { Card, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
 // import load from "../../../../../../asset/images/image2.jpg";
 // // import { Card } from "@components/card/Card";
-import { HomeDropsData } from "./HomeDropsData.js";
+import { useState, useEffect } from 'react'
+// import { HomeDropsData } from "./HomeDropsData.js";
 import { HomeOptData } from "./HomeOptData.js";
 
 const HomeDrops = () => {
+    const [homeDropsData, setHomeDropsData] = useState([])
+
+    useEffect(() => {
+
+        const getHomeDropsData = async () => {
+            const homeDropsDataFromServer = await fetchHomeDropsData()
+            setHomeDropsData(homeDropsDataFromServer)
+        }
+
+        getHomeDropsData()
+    }, []);
+
+    const fetchHomeDropsData = async () => {
+        const res = await fetch('http://localhost:5000/homeDropsData')
+        const data = await res.json()
+
+        return data
+    }
     const responsive = {
         desktop: {
-            breakpoint: {max: 3000, min: 1024,},
-            items:4
+            breakpoint: { max: 3000, min: 1024, },
+            items: 4
         },
         tablet: {
-            breakpoint: {max: 1024, min: 464},
+            breakpoint: { max: 1024, min: 464 },
             item: 3
         },
         mobile: {
-            breakpoint: {max: 464, min: 0},
-        items: 1
+            breakpoint: { max: 464, min: 0 },
+            items: 1
         }
     };
 
     return (
-        <div style={{width: '100%', position: 'relative',}}>
+        <div style={{ width: '100%', position: 'relative', }}>
             <div className="section">
                 <div className="d-flex justify-content-between align-items-center"
                     style={{ width: '100%', height: 70, }}
@@ -36,7 +55,7 @@ const HomeDrops = () => {
                 </div>
                 <hr style={{ height: '1px', }} />
             </div>
-             <div className="multi-carousel">
+            <div className="multi-carousel">
 
                 <Carousel
                     additionalTransfrom={0}
@@ -67,14 +86,14 @@ const HomeDrops = () => {
                     slidesToSlide={2}
                     swipeable
                 >
-                    {HomeDropsData.map((item, index) => {
+                    {homeDropsData.map((item, index) => {
                         return (
                             <Card
                                 key={index}
                                 style={{
                                     width: '15rem',
                                     padding: 20,
-                                    
+
                                 }}
                             >
                                 <img
@@ -86,16 +105,26 @@ const HomeDrops = () => {
                                         textAlign: 'center'
                                     }}
                                 />
-                                <CardBody>
+                                <CardBody className="p-0 mt-2">
                                     <CardTitle tag="h5">
                                         {item.nftName}
                                     </CardTitle>
                                     <CardSubtitle
-                                        className="mb-2 text-muted"
+                                        className="mb-2"
                                         tag="h6"
                                     >
-                                        {item.collectorName}
+                                        <div className="d-flex align-items-center"
+                                            style={{ height: 50, width: '100%', }}>
+                                            <div style={{ marginRight: '16px', }}>
+                                                <img src={item.nft} alt="" style={{ height: 25, width: 25, borderRadius: '50%' }} />
+                                            </div>
+                                            <div className="">                            
+                                                <p style={{ fontSize: 15, fontFamily: 'nunito' }}>{item.collectorName}</p>
+                                            </div>
+                                        </div>
+                                        
                                     </CardSubtitle>
+                                   
 
                                 </CardBody>
                             </Card>
@@ -104,7 +133,7 @@ const HomeDrops = () => {
 
                 </Carousel>
             </div>
-             <div className="section">
+            <div className="section">
                 <div className="categories">
                     <div className="categories-content">
                         {HomeOptData.map((item, index) => {
